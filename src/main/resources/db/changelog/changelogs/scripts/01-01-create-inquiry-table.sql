613bbd4071ec9784850eeeb9eb782dd851f79197
Veselov Nikolay <veselovnd@gmail.com>
2023-11-28 16:59:14 +0300

    feature/1: rework liquibase, inquiry table

--- src/main/resources/db/changelog/changelogs/scripts/01-01-create-inquiry-table.sql
+++ src/main/resources/db/changelog/changelogs/scripts/01-01-create-inquiry-table.sql
@@ -1,6 +1,9 @@
+CREATE
+    EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE inquiry
 (
-    inquiry_id  serial PRIMARY KEY,
+    inquiry_id  UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
     DATE        timestamp,
     customer_id int8 REFERENCES public.customer (id),
     division_id VARCHAR(255)
