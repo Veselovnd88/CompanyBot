07405029da487b2ffa0f819b55e17e751a1b4736
Veselov Nikolay <veselovnd@gmail.com>
2023-12-01 15:43:20 +0300

    feature/6: test

---
+++ src/test/java/ru/veselov/companybot/cache/impl/UserDataCacheFacadeImplTest.java
@@ -0,0 +1,83 @@
+package ru.veselov.companybot.cache.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.veselov.companybot.bot.BotState;
+import ru.veselov.companybot.cache.ContactCache;
+import ru.veselov.companybot.cache.InquiryCache;
+import ru.veselov.companybot.cache.UserStateCache;
+import ru.veselov.companybot.model.DivisionModel;
+import ru.veselov.companybot.util.TestUtils;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class UserDataCacheFacadeImplTest {
+
+    @Mock
+    ContactCache contactCache;
+
+    @Mock
+    InquiryCache inquiryCache;
+
+    @Mock
+    UserStateCache userStateCache;
+
+    @InjectMocks
+    UserDataCacheFacadeImpl userDataCacheFacade;
+
+    @Test
+    void shouldCallInternalCachesToClear() {
+        userDataCacheFacade.clear(TestUtils.USER_ID);
+
+        verify(contactCache).clear(TestUtils.USER_ID);
+        verify(userStateCache).clear(TestUtils.USER_ID);
+        verify(inquiryCache).clear(TestUtils.USER_ID);
+    }
+
+    @Test
+    void shouldCallUserStateCache() {
+        userDataCacheFacade.getUserBotState(TestUtils.USER_ID);
+
+        verify(userStateCache).getUserBotState(TestUtils.USER_ID);
+    }
+
+    @Test
+    void shouldCallUserStateCacheToSetUp() {
+        userDataCacheFacade.setUserBotState(TestUtils.USER_ID, BotState.BEGIN);
+
+        verify(userStateCache).setUserBotState(TestUtils.USER_ID, BotState.BEGIN);
+    }
+
+    @Test
+    void shouldCreateInquiryByInquiryCache() {
+        DivisionModel division = TestUtils.getDivision();
+        userDataCacheFacade.createInquiry(TestUtils.USER_ID, division);
+
+        verify(inquiryCache).createInquiry(TestUtils.USER_ID, division);
+    }
+
+    @Test
+    void shouldGetInquiryByInquiryCache() {
+        userDataCacheFacade.getInquiry(TestUtils.USER_ID);
+
+        verify(inquiryCache).getInquiry(TestUtils.USER_ID);
+    }
+
+    @Test
+    void shouldCreateContactByContactCache() {
+        userDataCacheFacade.createContact(TestUtils.USER_ID);
+
+        verify(contactCache).createContact(TestUtils.USER_ID);
+    }
+
+    @Test
+    void shouldGetContactByContactCache() {
+        userDataCacheFacade.getContact(TestUtils.USER_ID);
+
+        verify(contactCache).getContact(TestUtils.USER_ID);
+    }
+}
--- src/test/java/ru/veselov/companybot/util/TestUtils.java
+++ src/test/java/ru/veselov/companybot/util/TestUtils.java
@@ -1,7 +1,23 @@
 package ru.veselov.companybot.util;
 
+import ru.veselov.companybot.model.DivisionModel;
+
+import java.util.UUID;
+
 public class TestUtils {
 
-    public static Long botId = 1L;
+    public static Long BOT_ID = 1L;
+
+    public static final Long USER_ID = 2L;
+
+    public static final String DIVISION_NAME = "NAME";
+
+
+    public static DivisionModel getDivision() {
+        return DivisionModel.builder()
+                .divisionId(UUID.randomUUID())
+                .name(DIVISION_NAME)
+                .build();
+    }
 
 }
