a58a7d28c5791e7e686d1ed86f91412f52520c07
Veselov Nikolay <veselovnd@gmail.com>
2023-12-15 09:50:16 +0300

    feature/23: added details

--- src/test/java/ru/veselov/companybot/config/PostgresTestContainersConfiguration.java
+++ src/test/java/ru/veselov/companybot/config/PostgresTestContainersConfiguration.java
@@ -1,16 +1,17 @@
 package ru.veselov.companybot.config;
 
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-@TestConfiguration
+@TestConfiguration(proxyBeanMethods = false)
 public class PostgresTestContainersConfiguration {
 
     @Bean
-    @ServiceConnection
+    @ServiceConnection(type = JdbcConnectionDetails.class)
     public PostgreSQLContainer<?> postgresContainer(DynamicPropertyRegistry registry) {
         registry.add("spring.liquibase.enabled", () -> true);
         return new PostgreSQLContainer<>("postgres:16").withDatabaseName("cbotDB");
