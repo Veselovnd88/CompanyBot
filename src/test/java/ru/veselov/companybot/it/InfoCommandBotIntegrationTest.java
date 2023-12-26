bfd85b3a8eb664e4b450553fcdaf103f66827a7c
Veselov Nikolay <veselovnd@gmail.com>
2023-12-26 17:06:35 +0300

    bugfix/32: checked and tested INFO flow

--- src/test/java/ru/veselov/companybot/it/InfoCommandBotIntegrationTest.java
+++ src/test/java/ru/veselov/companybot/it/InfoCommandBotIntegrationTest.java
@@ -1,22 +1,27 @@
 package ru.veselov.companybot.it;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.veselov.companybot.bot.CompanyBot;
 import ru.veselov.companybot.bot.handler.TelegramFacadeUpdateHandler;
+import ru.veselov.companybot.cache.UserStateCache;
 import ru.veselov.companybot.config.BotConfig;
 import ru.veselov.companybot.config.PostgresTestContainersConfiguration;
+import ru.veselov.companybot.repository.CustomerRepository;
 import ru.veselov.companybot.util.MessageUtils;
 import ru.veselov.companybot.util.UserActionsUtils;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
 @ActiveProfiles("test")
 class InfoCommandBotIntegrationTest extends PostgresTestContainersConfiguration {
 
@@ -28,6 +33,18 @@ class InfoCommandBotIntegrationTest extends PostgresTestContainersConfiguration 
 
     @Autowired
     TelegramFacadeUpdateHandler telegramFacadeUpdateHandler;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    UserStateCache userStateCache;
+
+    @AfterEach
+    void clear() {
+        customerRepository.deleteAll();
+        userStateCache.reset();
+    }
 
     @Test
     void shouldReturnSendMessageWithInfo() {
